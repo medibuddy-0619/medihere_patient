5848c4e4712f92fb92df5d27885153ef079aa78c
Kim Seongjae <>
2021-06-07 21:39:03 +1200

    firebase setting, router setting

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:medihere_patient/src/ui/register_screen/email_register_screen_view.dart';
+import 'package:medihere_patient/src/ui/register_screen/password_register_screen_view.dart';
 import 'package:medihere_patient/src/ui/start_screen/start_screen_view.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-      home: StartScreenView(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => StartScreenView(),
+        '/emailRegister':(context) => EmailRegisterScreenView(),
+        '/passwordRegister':(context) => PasswordRegisterScreenView(),
+      },
     );
   }
 }
---
+++ lib/src/widgets/custom_base_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomBaseView extends StatelessWidget {
+  final AppBar? appBar;
+  final Widget body;
+  const CustomBaseView({Key? key, this.appBar, required this.body}) : super(key: key);@override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: appBar,
+          body: body,
+        ),
+      ),
+    );
+  }
+}
