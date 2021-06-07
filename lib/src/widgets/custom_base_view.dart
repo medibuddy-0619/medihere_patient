016dff0b2a8e9d1ab8524c1e3b6539d65e18e61f
Kim Seongjae <>
2021-06-07 22:18:23 +1200

    setting android

--- lib/src/widgets/custom_base_view.dart
+++ lib/src/widgets/custom_base_view.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 class CustomBaseView extends StatelessWidget {
   final AppBar? appBar;
   final Widget body;
-  const CustomBaseView({Key? key, this.appBar, required this.body}) : super(key: key);@override
+  const CustomBaseView({Key? key, this.appBar, required this.body}) : super(key: key);
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
